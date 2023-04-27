f06b90cc565c0c249fbe45d9ea187e0f37f0cd46
sohaib <sohaibtahir@gmail.com>
2023-04-27 22:45:03 +0100

    Added EndpointTests

---
+++ jpmc-task/jpmc-taskTests/Data/EndpointTests.swift
@@ -0,0 +1,28 @@
+//
+//  EndpointTests.swift
+//  jpmc-taskTests
+//
+//  Created by Sohaib Tahir on 27/04/2023.
+//
+
+import XCTest
+@testable import jpmc_task
+
+class EndpointTests: XCTestCase {
+    
+    func testMakeURL() {
+        let endpoint = PlanetsEndpoint.allPlanets
+        let expectedURL = URL(string: "https://swapi.dev/api/planets")
+        
+        XCTAssertEqual(endpoint.makeURL(), expectedURL)
+    }
+    
+    func testEquatable() {
+        let endpoint1 = PlanetsEndpoint.allPlanets
+        let endpoint2 = PlanetsEndpoint.allPlanets
+        let endpoint3 = PlanetsEndpoint.onePlanet(planetId: "1")
+        
+        XCTAssertTrue(endpoint1 == endpoint2)
+        XCTAssertFalse(endpoint1 == endpoint3)
+    }
+}
