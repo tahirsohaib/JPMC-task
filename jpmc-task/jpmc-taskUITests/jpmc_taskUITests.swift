3f62212425da2480602773cab9d969e8781bb821
sohaib <sohaibtahir@gmail.com>
2023-05-24 22:23:07 +0100

    Implement UI test in progress

---
+++ jpmc-task/jpmc-taskUITests/jpmc_taskUITests.swift
@@ -0,0 +1,41 @@
+//
+//  jpmc_taskUITests.swift
+//  jpmc-taskUITests
+//
+//  Created by Sohaib Tahir on 24/05/2023.
+//
+
+import XCTest
+
+class PlanetListUITests: XCTestCase {
+    var app: XCUIApplication!
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+        app = nil
+        try super.tearDownWithError()
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
